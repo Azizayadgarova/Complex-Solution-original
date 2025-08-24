d2cc5066c819341da13e520dbaaf06ecb5d11ad0
yadgarovtthjjiiiiiiiiiiiiiiiiiiii <mamuray819@gmail.com>
2025-08-24 22:35:31 +0500

    done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { useTranslation } from 'react-i18next';
-import coponylogo from '../assets/logo.png';
+import coponylogo from '../assets/logo.webp';
 
 const HOVER_COLOR = '#2a5e91';
 
